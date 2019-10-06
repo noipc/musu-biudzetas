347f6b9e1775b4abce757d01262c346f3e64868c
Augustinas <augustinas.butkus.91@gmail.com>
2019-10-06 09:47:03 +0300

    rm staticdir settings

--- musubiudzetas/settings.py
+++ musubiudzetas/settings.py
@@ -159,9 +159,6 @@ USE_TZ = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
