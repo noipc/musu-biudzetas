9cda4a537fb6bcc0262d7de930bf69573a5e45d4
augis911 <augustinas.butkus.91@gmail.com>
2019-10-07 02:21:22 +0300

    new tests files for deployment

--- config/start.sh
+++ config/start.sh
@@ -9,4 +9,4 @@ echo "==> Django setup, executing: migrate"
 python manage.py migrate
 
 echo "==> Staring wsgi ... "
-gunicorn musubiudzetas.wsgi:application --bind :8080 --workers 3
+gunicorn musubiudzetas.wsgi:application --bind :8080 --workers 5
