ed7b11fe51c3447edcc66087da734a25078e58fb
augis911 <augustinas.butkus.91@gmail.com>
2019-10-08 03:43:54 +0300

    test56

--- config/start.sh
+++ config/start.sh
@@ -9,4 +9,4 @@ echo "==> Django setup, executing: migrate"
 python manage.py migrate
 
 echo "==> Staring wsgi ... "
-gunicorn musubiudzetas.wsgi:application --bind :8080 --workers 5
+gunicorn musubiudzetas.wsgi --bind 0.0.0.0:8080 --workers 3
